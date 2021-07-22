e8fabb506e18ea0a06e57cf163856ea9b79b9de1
daotuan1139 <daotuan1139@gmail.com>
2021-07-22 18:02:07 +0700

    update asm

--- src/App/NavBar.js
+++ src/App/NavBar.js
@@ -5,47 +5,49 @@ import { Button } from 'react-bootstrap';
 
 const style = {
     color: 'white',
-    paddingLeft: 250,
-    display: 'flex',
 }
 const listyle = {
-    margin: 45
+    margin: 45,
+    display:'inline-block'
 }
 const Navbar = ({ isUserLoggedIn, logout }) => {
     return (
-        <div className="top-nav">
-            <ul style={style} >
-                <li style={listyle}>
-                    <Link to="/">Home page</Link>
-                </li>
-                <li style={listyle}>
-                    <Link to="/posts">Posts page</Link>
-                </li>
-                <li style={listyle}>
-                    <Link to="/profile">Profile page</Link>
-                </li>
-                <li style={listyle}>
-                    <Link to="/register">Register page</Link>
-                </li>
-                <li style={listyle}>
-                    {/* <Link to="/login">Login page</Link> */}
-                    {!isUserLoggedIn && (
-                        <Link to="/login">Login page</Link>
-                    )}
-                    {isUserLoggedIn && (
-                        <div>
-                            <Button style={{ paddingBottom: 0, paddingTop: 0 }} className="btn-primary" onClick={() => {
-                                logout();
-                                axios.defaults.headers.common['Authorization'] = '';
-                            }}
-                            >
-                                Logout
-                            </Button>
-                            <Redirect to='/profile' />
-                        </div>
-                    )}
-                </li>
-            </ul>
+        <div>
+            <center>
+
+                <ul style={style} >
+                    <li style={listyle}>
+                        <Link to="/">Home page</Link>
+                    </li>
+                    <li style={listyle}>
+                        <Link to="/posts">Posts page</Link>
+                    </li>
+                    <li style={listyle}>
+                        <Link to="/profile">Profile page</Link>
+                    </li>
+                    <li style={listyle}>
+                        <Link to="/register">Register page</Link>
+                    </li>
+                    <li style={listyle}>
+                        {/* <Link to="/login">Login page</Link> */}
+                        {!isUserLoggedIn && (
+                            <Link to="/login">Login page</Link>
+                        )}
+                        {isUserLoggedIn && (
+                            <div>
+                                <Button style={{ paddingBottom: 0, paddingTop: 0 }} className="btn-primary" onClick={() => {
+                                    logout();
+                                    axios.defaults.headers.common['Authorization'] = '';
+                                }}
+                                >
+                                    Logout
+                                </Button>
+                                <Redirect to='/profile' />
+                            </div>
+                        )}
+                    </li>
+                </ul>
+            </center>
         </div>
     );
 };
